96a5a439130d627737bbbdabf87afac05aa30541
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-03 13:58:56 +0200

    correccion script.js lineas 1&2 + recarga de productos al hacer post en myProducts.js lineas 18&19

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-import { getAllProducts, clearCards }  from './utils/data.js'
-import { paintCard } from './utils/paintData.js'
+import { getAllProducts } from './utils/data.js'
+import { paintCard, clearCards } from './utils/paintData.js'
 
 // Con init invocamos la función que borra las tarjetas y la que pide los productos a la API 
 // luego iteramos el array recibido y pintamos cada elemento con la función paintCard()
-const init = async () =>{
+const init = async() => {
     //...
     await clearCards()
     const allProducts = await getAllProducts()
